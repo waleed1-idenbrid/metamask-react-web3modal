b62c5be4e3ea5db87dbfdf85e42d1f953583f40a
Waleed <waleed1.idenbrid@gmail.com>
2023-09-20 18:02:20 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useAccount, useDisconnect } from "wagmi";
+import "./App.css";
+import { useWeb3Modal } from "@web3modal/react";
+function App() {
+  const { open } = useWeb3Modal();
+  const { isConnected, address } = useAccount();
+  const { disconnect } = useDisconnect();
+  return (
+    <>
+      <div></div>
+
+      <h1>Metamask + React</h1>
+      {!isConnected && (
+        <div className="card">
+          <button onClick={open}>Connect Wallet</button>
+          <p>Click on connect wallet to connect the wallet</p>
+        </div>
+      )}
+      {isConnected && (
+        <div className="card">
+          <p className="address">{address}</p>
+          <button onClick={disconnect}>Disconnect Wallet</button>
+        </div>
+      )}
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
+const projectId = "881db498bedc3f8a64adae204e7341c4";
+
+const chains = [arbitrum, mainnet, polygon];
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient,
+});
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <WagmiConfig config={wagmiConfig}>
+      <App />
+    </WagmiConfig>
+    <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+  </React.StrictMode>
+);
